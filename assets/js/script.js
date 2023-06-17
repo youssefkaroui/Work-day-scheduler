73b3c0048754bd1e6fab013d42ab30539990df42
Youssef <youssefkaroui6@gmail.comm>
2023-06-17 10:03:46 -0400

    currentday displaying properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,12 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+$ (document).ready(function  () {
+
+  var currentDayEl = $('#currentDay');
+   var currentday=dayjs().format('dddd, MMM DD, YYYY');
+   console.log(currentday);
+   currentDayEl.text("Today is " +currentday);
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
