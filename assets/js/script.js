718b77e41cc05f3512b566de36a25f3cc2c06328
Youssef <youssefkaroui6@gmail.comm>
2023-06-23 10:48:04 -0400

    all the content is wrapped in a call to jQuery, color red for present time block is now working after converting string to interger

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,22 +11,27 @@ $ (document).ready(function  () {
    console.log(timeOfTheDay);
    currentDayEl.text("Today is " +currentday);
 
+   // checks the current hour and changes the timeblock backgroung color accordingly
+
    function checkTime (){
      
-     
      for (var i=9; i<=17; i++){
+       var currentHour= parseInt(timeOfTheDay);
        var timeBlock=$("#hour-"+i)
-       if (timeOfTheDay>i) {
+       if (currentHour>i) {
          $(timeBlock).addClass("past");
-       } else if (timeOfTheDay===i){
+       } else if (currentHour === i){
          $(timeBlock).addClass("present");
-       }  else{
+       }  else {
          $(timeBlock).addClass("future");
        }
      }
      
    }
-   
+  
+
+
+
    checkTime()
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -35,16 +40,12 @@ $ (document).ready(function  () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+ 
   
 });
